6381f6bf134ec334f3fffe5c82bb697d7354adb1
fredrick-gachihi <fredrickgachihi24@gmail.com>
2024-04-14 00:34:48 +0300

    my looping lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      const name = names[i];
+      const message = `Thank you, ${name}, for the wonderful surprise gift!`;
+      thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+}
+function countDown(startNumber) {
+    for (let p = startNumber; p >= 0; p--) { 
+      console.log(p);
+    }
+  }
